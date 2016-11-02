0348c4b7093de63d4fd72128aacc31b3104d19fe
John Curry <john.steven.curry@gmail.com>
2016-11-02 12:42:31 -0700

    added formDisplay class to refactor the search form page

---
+++ models/FormDisplay.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Jsc_dont_waste;
+
+class FormDisplay {
+
+    public function all_conditions_heading(){
+        $headingText = "<h3>Meet ALL Conditions Search</h3>";
+        $headingText .= "<p>Check all the categories that an activity MUST be in. If you wanted a solo indoor physical activity you could "; 
+        $headingText .= "check <strong>'solo', 'indoor', and 'physical'</strong> to only get activities that are Solo, Indoor AND physical.</p>";
+
+        return $headingText;
+    }
+
+    public function any_conditions_heading(){
+        $headingText = "<h3>Meet ANY Conditions Search</h3>";
+        $headingText .= "<p>Check any category that you want to include. This search is an \"EITHER OR\" search.";
+        $headingText .= "If you want something that's either X OR Y or Z or ..., then select all categories you want to include.</p>";
+
+        return $headingText;
+    }
+
+    public function generate_conditions_form($formValue){
+
+        if ($formValue == 'all') {
+            $conditionsType = 'meet_all_conditions';
+        } else if ($formValue == 'any') {
+            $conditionsType = 'meet_any_conditions';
+        }
+
+        $args = array(
+            'hide_empty' => true
+        );
+
+        $terms = get_terms('jsc_activity_cat', $args);
+
+        $htmlForm = '<form action="' . esc_url( admin_url('admin-post.php') ) .'" method="get">
+        <input type="hidden" name="action" value="' . $conditionsType . '">';
+
+        foreach( $terms as $theTerm ) {
+            $vars = get_object_vars($theTerm);
+            $htmlForm .= '<label><input type="checkbox" name="activities[]" id="'. $vars['term_id'] . '" value="'. $vars['term_id'] . '"> ' . $vars['name'] . '</label>'; 
+        }
+
+        $htmlForm .= '<input type="submit" value="get random activity!"></form>';
+
+        return $htmlForm;
+    }
+
+}
+
+
+
+?>
--- searchActivities.php
+++ searchActivities.php
@@ -2,62 +2,26 @@
 
 /******
 
-This entire page has two forms. Each form has their own callback that is created via their hidden field types. 
+This admin page has two forms. Each form has their own callback that is created via their hidden field types. 
 Each form generates all the "activity categories" and allows users to select which categories to include in their search.
-The search is handled via the "DataObject class"
+The search is handled via the "DataObject class" and the form display is handled in FormDisplay.php
 
 ******/
+include JSC_DONT_WASTE . '/models/FormDisplay.php';
+$formObj = new Jsc_dont_waste\FormDisplay();
 
-echo "<h3>Meet ALL Conditions Search</h3>";
-echo "<p>Check all the categories that an activity MUST be in. If you wanted a solo indoor physical activity you could ". 
-"check <strong>'solo', 'indoor', and 'physical'</strong> to only get activities that are Solo, Indoor AND physical.</p>";
-
-$args = array(
-    'hide_empty' => true
-);
-
-$terms = get_terms('jsc_activity_cat', $args);
+echo $formObj->all_conditions_heading();
+echo $formObj->generate_conditions_form('all');
 ?>
 
-<form action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="get">
-    <input type="hidden" name="action" value="meet_all_conditions">
-<?php
+<br>
+<br>
+<br>
+<br>
+<br>
 
-$htmlForm = '';
 
-foreach( $terms as $theTerm ) {
-    $vars = get_object_vars($theTerm);
-    $htmlForm .= '<label><input type="checkbox" name="activities[]" id="'. $vars['term_id'] . '" value="'. $vars['term_id'] . '"> ' . $vars['name'] . '</label>'; 
-}
-
-echo $htmlForm;
-
+<?php 
+echo $formObj->any_conditions_heading();
+echo $formObj->generate_conditions_form('any');
 ?>
-
-<input type="submit" value="get random activity!">
-</form>
-<br>
-<br>
-<br>
-<br>
-<br>
-
-<h3>Meet ANY Criteria Search</h3>
-<p>Check any category that you want to include. This search is an "EITHER OR" search. If you want something that's either X OR Y or Z or ..., then select all categories you want to include.</p>
-
-<form action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="get">
-    <input type="hidden" name="action" value="meet_any_conditions">
-<?php
-
-$htmlForm2 = '';
-
-foreach( $terms as $theTerm ) {
-    $vars = get_object_vars($theTerm);
-    $htmlForm2 .= '<label><input type="checkbox" name="activities[]" id="'. $vars['term_id'] . '" value="'. $vars['term_id'] . '"> ' . $vars['name'] . '</label>'; 
-}
-
-echo $htmlForm2;
-?>
-
-<input type="submit" value="get random activity!">
-</form> <br>
